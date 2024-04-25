29709f2ad7c20e22d97696193ff54bdae08aac95
Chris Farah <farahca@mail.uc.edu>
2024-04-25 19:59:25 -0400

    ew

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const button = ({children}) => {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default button
---
+++ src/constansts/index.js
@@ -0,0 +1,24 @@
+
+export const navigation = [
+  {
+    id: "0",
+    title: "Home",
+    url: "#Home",
+  },
+  {
+    id: "1",
+    title: "About",
+    url: "#About",
+  },
+  {
+    id: "2",
+    title: "Projects",
+    url: "#Projects",
+  },
+  {
+    id: "3",
+    title: "Contact",
+    url: "#Contact",
+  },
+];
+
