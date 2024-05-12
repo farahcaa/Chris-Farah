c53e5a8ddba04269c56e2a0b09815e8402154699
Chris Farah <farahca@mail.uc.edu>
2024-05-12 13:19:56 -0400

    f

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,15 @@ import Section from "./Section";
 const Projects = () => {
   return (
     <Section id="Projects" className="bg-B" crossescolor={true}>
-      <div className="flex justify-center items-center text-Bo">adsfsadf</div>
+      <div className="flex flex-col justify-center items-center text-Bo">
+        <h1 className="h1">Project Section under dev</h1>
+        <a
+          className="text-Bs text-xl underline"
+          href="farahcaa.github.io/tailWind"
+        >
+          Project 1
+        </a>
+      </div>
     </Section>
   );
 };
