40b7bef466c8ad2b0c89205c21f6c98a3a77db0d
farahcaa <144845898+farahcaa@users.noreply.github.com>
2024-06-20 13:35:35 -0400

    started actual project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,22 +9,30 @@ const Projects = () => {
       crossesOffset={`lg:translate-y-[5.25rem]`}
       crossescolor={true}
     >
-      <div className="flex flex-col justify-center items-center text-Bo">
-        <h1 className="h1 capitalize">Project Section under dev</h1>
-        <a
-          className="text-Bs text-[2rem] p-10 underline"
-          target="_blank"
-          href="https://farahcaa.github.io/tailWind"
-        >
-          Project 1 react website
-        </a>
-        <a
-          className="text-Bs text-[2rem] p-10 underline"
-          target="_blank"
-          href="https://banking-app-psi-amber.vercel.app/"
-        >
-          Project 2 next.js banking application
-        </a>
+      <h1 className="flex justify-center pb-5 h1 text-Bs">Projects</h1>
+      <div className="flex justify-center items-center border border-black">
+        <div className="flex border border-black pr-5 w-1/2 h-full ">
+          <div
+            href="https://banking-app-psi-amber.vercel.app/"
+            className=" rounded-2xl border border-Bs overflow-hidden w-full"
+          >
+            <img
+              src="./src/assets/HorizonImage.svg"
+              className="w-full h-full "
+            />
+          </div>
+        </div>
+        <div className="flex flex-col justify-center border w-1/2 h-1/4 border-black">
+          <div
+            href="https://farahcaa.github.io/tailWind"
+            className="w-1/2 h-[100px] overflow-hidden stretch border border-Bs rounded-2xl"
+          >
+            <img src="./src/assets/robot.jpg" className="w-full h-full" />
+          </div>
+          <div className="w-1/2 h-1/4 overflow-hidden border border-Bs rounded-2xl">
+            <img src="./src/assets/robot.jpg" className="w-full h-full" />
+          </div>
+        </div>
       </div>
     </Section>
   );
