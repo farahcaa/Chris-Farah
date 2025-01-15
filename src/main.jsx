6245842fd80bd5636ad9c8c9c1cc13378f6c62ab
Chris Farah <farahca@mail.uc.edu>
2025-01-15 18:47:30 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Blog from "./Pages/BlogPage/Blog.jsx";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/blog" element={<Blog />} />
       </Routes>
 
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
