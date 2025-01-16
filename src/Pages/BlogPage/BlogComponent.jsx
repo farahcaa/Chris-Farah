91aca407a2c9166f82d43f706f5e2be516e146b7
Chris Farah <farahca@mail.uc.edu>
2025-01-15 20:57:52 -0500

    Update BlogComponent.jsx

--- src/Pages/BlogPage/BlogComponent.jsx
+++ src/Pages/BlogPage/BlogComponent.jsx
@@ -5,7 +5,7 @@ const BlogComponent = () => {
     <div className='mt-20 flex border border-white h-[75vh]'>
         <div className='flex justify-center items-center w-1/2 '>
             <h1 className='text-center flex items-center justify-center w-1/2'>welcome to the first of many blogs Here I will be talking about whatever the professor assigns and putting a
-            cute little picture on the side of it </h1>
+            cute little picture on the side of its </h1>
         </div>
         <div></div>
     </div>
