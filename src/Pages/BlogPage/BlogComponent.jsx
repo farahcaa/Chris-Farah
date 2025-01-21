107af26c3a4d2cae373cd5b2c30dc35835dfa5d2
farahcaa <144845898+farahcaa@users.noreply.github.com>
2025-01-21 13:44:30 -0500

    xs

--- src/Pages/BlogPage/BlogComponent.jsx
+++ src/Pages/BlogPage/BlogComponent.jsx
@@ -1,13 +1,13 @@
 const BlogComponent = ({ text, title, img }) => {
   return (
-    <div className=" my-20 flex lg:flex-row md:flex-col">
-      <div className="flex justify-center flex-col items-center lg:w-1/2 md:w-full ">
-        <h1 className="text-4xl font-bold my-3">{title}</h1>
+    <div className=" my-20 flex lg:flex-row xs:flex-col">
+      <div className="flex justify-center flex-col items-center lg:w-1/2 xs:w-full ">
+        <h1 className="text-4xl text-center font-bold my-3">{title}</h1>
         <h1 className="text-center flex items-center justify-center w-3/4">
           {text}
         </h1>
       </div>
-      <div className="flex justify-center items-center lg:w-1/2 md:w-full">
+      <div className="flex justify-center items-center lg:w-1/2 xs:w-full">
         <img src={img} alt="blog" className="flex w-4/6 shadow-xl rounded-lg" />
       </div>
     </div>
