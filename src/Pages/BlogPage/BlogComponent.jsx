77ce1962810a78119cb38de210dc17aed285355e
Chris Farah <farahca@mail.uc.edu>
2025-04-07 19:47:27 -0400

    padding fix

--- src/Pages/BlogPage/BlogComponent.jsx
+++ src/Pages/BlogPage/BlogComponent.jsx
@@ -3,7 +3,7 @@ const BlogComponent = ({ text, title, img }) => {
     <div className=" my-20 flex lg:flex-row xs:flex-col">
       <div className="flex justify-center flex-col items-center lg:w-1/2 xs:w-full ">
         <h1 className="text-4xl text-center font-bold my-3">{title}</h1>
-        <h1 className="text-center flex items-center justify-center w-3/4">
+        <h1 className="text-center flex items-center justify-center px-7">
           {text}
         </h1>
         {title == "Blog 04 Digital Ad" && (
