f182f1b2533a8df44d9273c334fd57fd45df74fb
Chris <farahca@mail.uc.edu>
2026-05-04 09:41:16 +0200

    fix ssr

--- pages/+config.ts
+++ pages/+config.ts
@@ -2,7 +2,7 @@ import type { Config } from "vike/types";
 import vikeReact from "vike-react/config";
 
 export default {
-  ssr: false,
+  ssr: true,
   prerender: true,
   extends: [vikeReact],
   title: "Christopher Farah",
